8245781af92ea3cfc6662b0863e6479a844b342e
Ahmed Rafie <91790739+a1b1f@users.noreply.github.com>
2023-11-23 11:01:36 +0200

    Add files via upload

---
+++ Core/Models/Record.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Record
+    {
+        public int Id { get; set; }
+        public string PatientName { get; set; }
+        public string Symptoms { get; set; }
+        public string Diagnosis { get; set; }
+        public string TreatmentPlan { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
