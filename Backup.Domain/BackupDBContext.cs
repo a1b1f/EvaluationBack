be3ddbbdfdc6630a57c7cb98124279b6e26bdad4
Ahmed Rafie <91790739+a1b1f@users.noreply.github.com>
2023-11-23 11:00:53 +0200

    Add files via upload

---
+++ Backup.Domain/BackupDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backup.Domain
+{
+    public class BackupDBContext :DbContext
+    {
+        public BackupDBContext(DbContextOptions<BackupDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Records> BackUpRecords { get; set; }
+    }
+}
---
+++ Backup.Domain/Migrations/20231122184307_ii.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backup.Domain.Migrations
+{
+    public partial class ii : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BackUpRecords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PatientName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Symptoms = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Diagnosis = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TreatmentPlan = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BackUpRecords", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BackUpRecords");
+        }
+    }
+}
