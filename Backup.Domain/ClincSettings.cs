a6ec024ac7dc5957c6da5ae862f86547cc5e5565
Ahmed Rafie <91790739+a1b1f@users.noreply.github.com>
2023-11-23 16:03:24 +0200

    Add files via upload

---
+++ Backup.Domain/ClincSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backup.Domain
+{
+    public class ClincSettings
+    {
+        public string ConnectionStrings { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
+        public string RecordCollection { get; set; } = string.Empty;
+    }
+}
